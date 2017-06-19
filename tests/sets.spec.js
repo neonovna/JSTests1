64cb16464fbf33740dbd22445048c796ca38afc9
neona <neona@neona-Vostro-3500>
2017-06-19 09:08:10 +0200

    dd

--- tests/sets.spec.js
+++ tests/sets.spec.js
@@ -5,53 +5,53 @@ var locator_sh = shop_page.locators;
 var locator = sets_page.locators;
 
 
-// describe("Add SETS Interests product to bag", function() {
-//   common.shop_go();
-//
-//   it("Open SETS content", function() {
-//     locator_sh.sets_btn.click();
-//     browser.driver.sleep(3000);
-//     expect(locator_sh.sets_modal.isDisplayed()).toBe(true);
-//   });
-//
-//   it("Click on Sets HOME btn", function() {
-//     locator.sets_home_btn.click();
-//     browser.driver.sleep(3000);
-//     expect(locator.sets_title.isDisplayed()).toBe(true);
-//   });
-//
-//   it("Open Interests page", function() {
-//     locator.all_interests_btn.click();
-//     browser.driver.sleep(3000);
-//     expect(locator.interests_nav_trail.isDisplayed()).toBe(true);
-//     expect(locator.interests_title.isDisplayed()).toBe(true);
-//     //check if view button is inactive
-//     expect(locator.view_selected_inactive.isDisplayed()).toBe(true);
-//   });
-//
-//   it("Select any Game", function() {
-//     locator.any_checkbox.click();
-//     browser.driver.sleep(2000);
-//     //vie selected button is active
-//     expect(locator.view_selected_active.isDisplayed()).toBe(true);
-//
-//   });
-//
-//   it("View Selected Interests", function() {
-//     //Pass protractor element to javascript code and click on it
-//     //We can use arguments[0].click() to simulate click operation.
-//     browser.executeScript("arguments[0].click()", locator.view_selected);
-//     browser.driver.sleep(3000);
-//     expect(locator.list_of_products.isDisplayed()).toBe(true);
-//   })
-//
-//   it("Add any game to Bag", function() {
-//     locator.add_to_bag_btn.click();
-//     browser.driver.sleep(2000);
-//     expect(locator.added_notif.isDisplayed()).toBe(true);
-//   });
-//
-// });
+describe("Add SETS Interests product to bag", function() {
+  common.shop_go();
+
+  it("Open SETS content", function() {
+    locator_sh.sets_btn.click();
+    browser.driver.sleep(3000);
+    expect(locator_sh.sets_modal.isDisplayed()).toBe(true);
+  });
+
+  it("Click on Sets HOME btn", function() {
+    locator.sets_home_btn.click();
+    browser.driver.sleep(3000);
+    expect(locator.sets_title.isDisplayed()).toBe(true);
+  });
+
+  it("Open Interests page", function() {
+    locator.all_interests_btn.click();
+    browser.driver.sleep(3000);
+    expect(locator.interests_nav_trail.isDisplayed()).toBe(true);
+    expect(locator.interests_title.isDisplayed()).toBe(true);
+    //check if view button is inactive
+    expect(locator.view_selected_inactive.isDisplayed()).toBe(true);
+  });
+
+  it("Select any Game", function() {
+    locator.any_checkbox.click();
+    browser.driver.sleep(2000);
+    //vie selected button is active
+    expect(locator.view_selected_active.isDisplayed()).toBe(true);
+
+  });
+
+  it("View Selected Interests", function() {
+    //Pass protractor element to javascript code and click on it
+    //We can use arguments[0].click() to simulate click operation.
+    browser.executeScript("arguments[0].click()", locator.view_selected);
+    browser.driver.sleep(3000);
+    expect(locator.list_of_products.isDisplayed()).toBe(true);
+  })
+
+  it("Add any game to Bag", function() {
+    locator.add_to_bag_btn.click();
+    browser.driver.sleep(2000);
+    expect(locator.added_notif.isDisplayed()).toBe(true);
+  });
+
+});
 
 
 describe("Add SETS Themes product to cart", function() {
